28fe22d186bb48f742a1b05f337366efe817b6df
Giacomo Leo Bertuccioli <giacomo.bertuccioli@studio.unibo.it>
2021-11-08 11:42:31 +0100

    Completed enum1 exercise.

--- src/it/unibo/oop/lab/enum1/Sport.java
+++ src/it/unibo/oop/lab/enum1/Sport.java
@@ -14,4 +14,21 @@ public enum Sport {
      * declare the following sports: - basket - soccer - tennis - bike - F1 -
      * motogp - volley
      */
+	BASKET("Basketball"),
+	SOCCER("Soccer"),
+	TENNIS("Tennis"),
+	BIKE("Biking"),
+	F1("Formula 1"),
+	MOTOGP("MotoGP"),
+	VOLLEY("Volleyball");
+	
+	private final String name;
+
+	private Sport(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
 }
